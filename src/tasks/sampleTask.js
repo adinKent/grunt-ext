eae1dbda0db8059af78a003b6c45448180b0dff1
Kent <kenoboe.luck@gmail.com>
2019-08-23 15:01:58 +0800

    Add jsdoc to project

--- src/tasks/sampleTask.js
+++ src/tasks/sampleTask.js
@@ -1,7 +1,17 @@
+/**
+ * Task sampleTask.
+ * @module sampleTask
+ */
 module.exports = {
   name: 'sampleTask',
   desc: 'It\'s a sample task',
-  task() {
+  /**
+   * Execute task
+   * @param {object} payload - The task's require data
+   * @return {string} The result of taksk.
+  */
+  task(payload) {
     console.log('Execute sample task');
+    return 'task reulst';
   },
 };
